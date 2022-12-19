8d70afbf8290dd0ec0fd96ee610e20f5d39da531
OviOvocny <werint.pp@gmail.com>
2022-12-19 22:09:17 +0100

    fix extension reading

--- resolvers/tls.py
+++ resolvers/tls.py
@@ -42,26 +42,26 @@ class TLS:
       result["protocol"] = sock.get_protocol_version_name()
       result["cert_chain"] = chain
     except socket.gaierror as e:
-      logger.error("Cant resolve domain name or connection error")
+      logger.error(f"{host} TLS: Cant resolve domain name or connection error")
       return None
     except socket.timeout as e:
-      logger.error("socket intimeout")
+      logger.error(f"{host} TLS: socket intimeout")
       return None
     except OpenSSL.SSL.Error as e:
-      logger.error("cannot find any root certificates")
+      logger.error(f"{host} TLS: cannot find any root certificates")
       return None
     except ConnectionRefusedError as e:
-      logger.error("connection refused")
+      logger.error(f"{host} TLS: connection refused")
       return None
     except OSError as e:
-      logger.error("built-in exception in Python")
+      logger.error(f"{host} TLS: built-in exception in Python")
       return None
 
     try:
       sock.shutdown()
       sock.close()
     except:
-      logger.error("Fatal error during socket closing")
+      logger.error(f"{host} TLS: Fatal error during socket closing")
       return None
 
     return result
@@ -73,14 +73,14 @@ class TLS:
     valid_from_raw = cert.get_notBefore()
     valid_to_raw = cert.get_notAfter()
     if valid_from_raw is None or valid_to_raw is None:
-      logger.error("Certificate validity is None")
+      logger.error(f"TLS: Certificate validity is None")
       valid_from = None
       valid_to = None
       validity_len = None
     else:
       valid_to = datetime.datetime.strptime(valid_to_raw.decode("utf-8")[:-1], "%Y%m%d%H%M%S")
       valid_from = datetime.datetime.strptime(valid_from_raw.decode("utf-8")[:-1], "%Y%m%d%H%M%S")
-      validity_len = valid_to - valid_from
+      validity_len = int((valid_to - valid_from).total_seconds())
     
     # Parse issuer info
     attributes = str(cert.get_issuer()).split('/')
@@ -88,13 +88,13 @@ class TLS:
     organization = None
     country = None
     for attr in attributes:
-      key, val = attr.split('=')
-      if key == 'CN':
-        common_name = val
-      elif key == 'O':
-        organization = val
-      elif key == 'C':
-        country = val
+      split = attr.split('=')
+      if split[0] == 'CN':
+        common_name = split[1]
+      elif split[0] == 'O':
+        organization = split[1]
+      elif split[0] == 'C':
+        country = split[1]
 
     # Parse extensions
     extensions: List[CertificateExtension] = []
@@ -103,7 +103,7 @@ class TLS:
       extensions.append({
         "critical": ext.get_critical(),
         "name": ext.get_short_name().decode("utf-8"),
-        "value": ext.get_data().decode("asn1")
+        "value": str(ext)
       })
     
     return Certificate(
