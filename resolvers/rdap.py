eaa59c85b3a88b03ccfc5798141fd357d5d821c4
ihranicky <ihranicky@fit.vutbr.cz>
2023-10-19 19:17:15 +0200

    Fixed RDAP resolvers in case of nonexisting attributes

--- resolvers/rdap.py
+++ resolvers/rdap.py
@@ -1,20 +1,23 @@
 """Self-contained RDAP/WHOIS resolver for the collector, wraps whoisit module with auto bootstrapping and falls back to
 whois if needed"""
-__author__ = "Adam Horák"
+__author__ = "Adam Horák, Ondřej Ondryáš"
 
 import re
 
 import dns.name
+import tldextract
 import whoisit
 from whoisit.errors import BootstrapError
-import whoisdomain as whois
+
+from dr_collector.config import Config
+import dr_collector.timing as timing
+from dr_collector.logger import logger_resolvers as logger
+from dr_collector.datatypes import RDAPDomainData, RDAPIPData, IPNetwork
+from dr_collector.exceptions import *
+from typing import Optional
 import json
 
-import timing
-from logger import logger
-from datatypes import RDAPDomainData, RDAPIPData, RDAPASNData, RDAPEntityData, IPNetwork
-from exceptions import *
-from typing import Optional
+import whoisdomain as whois
 
 
 class RDAP:
@@ -22,39 +25,42 @@ class RDAP:
         if not whoisit.is_bootstrapped():
             load_bootstrap_data()
 
+        whoisit.utils.http_timeout = Config.TIMEOUT
+        whoisit.utils.http_max_retries = 2
+        whoisit.utils.http_pool_maxsize = 32
+        whoisit.utils.http_pool_connections = 32
+
     @timing.time_exec
     def domain(self, domain: str, **kwargs) -> Optional[RDAPDomainData]:
         original_domain = domain
 
         def worker(current_domain):
             try:
-                logger.debug(f"Querying RDAP for {current_domain}")
                 return whoisit.domain(current_domain, **kwargs)
             except whoisit.errors.UnsupportedError:
-                logger.warning(f"No RDAP endpoint for {current_domain}")
+                logger.info(f"No RDAP endpoint for {current_domain}")
             except whoisit.errors.RateLimitedError:
                 logger.warning(f"RDAP rate limited for {current_domain}")
             except whoisit.errors.ResourceDoesNotExist:
-                logger.warning(f"RDAP resource doesn't exist: {current_domain}")
+                logger.info(f"RDAP resource doesn't exist: {current_domain}")
+            except whoisit.errors.QueryError as e:
+                logger.info(f"RDAP status code error for {current_domain}: {str(e)}")
             except Exception as e:
                 logger.error(f'RDAP error for {current_domain}', exc_info=e)
 
-            err = ResolutionImpossible()
             try:
                 return self._query_whois(current_domain)
             except (ResolutionNeedsRetry, ResolutionImpossible) as e:
                 err = e
 
-            dn = dns.name.from_text(current_domain)
-            if len(dn) <= 3:
+            extracted = tldextract.extract(current_domain)
+            if len(extracted.subdomain) == 0:
                 raise err
 
-            more_general_name = dn.split(len(dn) - 1)[1]
-            to_try = more_general_name.to_text(True)
-
+            more_general_name = extracted.registered_domain
             logger.info(f"Trying more general name {more_general_name} for {original_domain}")
 
-            return worker(to_try)
+            return worker(more_general_name)
 
         return worker(domain)
 
@@ -62,30 +68,30 @@ class RDAP:
     def _query_whois(domain: str) -> Optional[RDAPDomainData]:
         try:
             logger.info(f'Trying whois fallback for {domain}')
-            w = whois.query(domain)
+            w = whois.query(domain, timeout=Config.TIMEOUT)
             if w is not None:
                 return whois_to_rdap_domain(w)
         except whois.exceptions.WhoisQuotaExceeded:
             logger.error(f'Whois quota exceeded (at {domain})')
-            raise ResolutionNeedsRetry
+            raise ResolutionNeedsRetry()
         except whois.exceptions.UnknownTld:
-            logger.warning(f'Unknown TLD for {domain}')
-            raise ResolutionImpossible
+            logger.info(f'Unknown TLD for {domain}')
+            raise ResolutionImpossible()
         except whois.exceptions.WhoisPrivateRegistry:
             logger.warning(f'Whois private registry for {domain}')
-            raise ResolutionImpossible
+            raise ResolutionImpossible()
         except (whois.exceptions.WhoisCommandTimeout, whois.exceptions.WhoisCommandFailed):
             logger.warning(f'Whois timeout/fail for {domain}')
-            raise ResolutionNeedsRetry
+            raise ResolutionNeedsRetry()
         except whois.exceptions.FailedParsingWhoisOutput:
             logger.warning(f'Invalid whois output for {domain}')
-            raise ResolutionImpossible
+            raise ResolutionImpossible()
         except Exception as e:
             logger.error(f'Whois query for {domain} failed', exc_info=e)
-            raise ResolutionImpossible
-        
-        logger.warning(f'Whois empty for {domain}')
-        raise ResolutionNeedsRetry
+            raise ResolutionImpossible()
+
+        logger.info(f'Whois empty for {domain}')
+        raise ResolutionImpossible()
 
     @timing.time_exec
     def ip(self, ip: str, **kwargs) -> Optional[RDAPIPData]:
@@ -164,16 +170,16 @@ def whois_to_rdap_domain(d: whois.Domain) -> RDAPDomainData:
         url='',
         entities={
             'registrant': [{
-                'name': definitely_string(d.registrant)
+                'name': definitely_string(d.registrant) if hasattr(d, 'registrant') else ''
             }],
             'abuse': [{
-                'email': definitely_string(d.abuse_contact)
+                'email': definitely_string(d.abuse_contact) if hasattr(d, 'abuse_contact') else ''
             }],
             'admin': [{
-                'name': definitely_string(d.admin)
+                'name': definitely_string(d.admin) if hasattr(d, 'admin') else ''
             }],
             'registrar': [{
-                'name': definitely_string(d.registrar)
+                'name': definitely_string(d.registrar) if hasattr(d, 'registrar') else ''
             }]
         },
         nameservers=[n.upper() for n in d.name_servers],
